999d1a9c6b587b2c5a5410e4395ea87275ecf415
Matthew Rougely <matthewrougelysr@gmail.com>
2020-07-10 14:41:22 -0700

    Complete
    Hosted on Heroku

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
@@ -44,7 +46,17 @@ app.get('/movie', function handleGetMovie(req, res) {
   res.json(response);
 });
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
